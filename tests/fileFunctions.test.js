160c9946a1d897c43eb0587013116085a568aa8e
ikm216 <ikmclassof22@gmail.com>
2024-12-05 22:07:00 -0500

    Starting over

---
+++ tests/fileFunctions.test.js
@@ -0,0 +1,7 @@
+const { writeFile, readFile, countLines } = require('../fileFunctions');
+const fs = require('fs');
+const path = require('path');
+
+
+const testFilePath = path.join(__dirname, 'testFile.txt');
+
