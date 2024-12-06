b5ca0609f9e04e70e44e3104601a526cf46e5724
ikm216 <ikmclassof22@gmail.com>
2024-12-05 22:17:03 -0500

    doc: added documentation  to write content on a file

--- fileFunctions.js
+++ fileFunctions.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
 
+/**
+ * Writes content to a file specified by the file path.
+ * 
+ * @param {string} filePath - Path to the file.
+ * @param {string} content - Content to write.
+ */
+
 function writeFile(filePath, content) {
     fs.writeFileSync(filePath, content, 'utf8');
   }
