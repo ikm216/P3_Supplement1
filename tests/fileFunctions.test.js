3af7bb98684fec25b85ce88bba4fc6c58c2f31c4
ikm216 <ikmclassof22@gmail.com>
2024-12-05 22:09:36 -0500

    test: revamp test to write content on a file

--- tests/fileFunctions.test.js
+++ tests/fileFunctions.test.js
@@ -5,3 +5,15 @@ const path = require('path');
 
 const testFilePath = path.join(__dirname, 'testFile.txt');
 
+describe('File Operations Tests', () => {
+  afterAll(() => {
+    if (fs.existsSync(testFilePath)) {
+      fs.unlinkSync(testFilePath);
+    }
+  });
+
+  test('should write content to a file', () => {
+    writeFile(testFilePath, 'Grind it Out!');
+    expect(fs.existsSync(testFilePath)).toBe(true);
+  });
+});
