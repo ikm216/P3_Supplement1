8958c049eef85df07119c80d5cce30c2b7645058
ikm216 <ikmclassof22@gmail.com>
2024-12-05 22:33:43 -0500

    feat: added feature to read content on a file

--- fileFunctions.js
+++ fileFunctions.js
@@ -11,4 +11,8 @@ function writeFile(filePath, content) {
     fs.writeFileSync(filePath, content, 'utf8');
   }
 
-  module.exports = { writeFile };
+function readFile(filePath) {
+    return fs.readFileSync(filePath, 'utf8');
+}
+
+  module.exports = { writeFile, readFile };
